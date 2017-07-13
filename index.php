cf0881203e2ea63d57cb492c66765044f843203f
aurinsomnia <auriskv@gmail.com>
2017-07-13 17:36:25 -0400

    "Adjusting home button to be active"

--- index.php
+++ index.php
@@ -89,7 +89,7 @@
             </div>
 
             <div class="col col-md-9">
-                        <a href="index.php"><div class="btn-primary btn navbutton" role="button">Home</div></a>
+                        <a href="index.php"><div class="btn active btn navbutton" role="button">Home</div></a>
                         <a href="about.html"><div class="btn btn-secondary navbutton" role="button">About Us</div></a>
                         <a href="mission.html"><div class="btn btn-secondary navbutton" role="button">Mission</div></a>
                         <a href="#"><div class="btn btn-secondary navbutton" role="button">Opportunities</div></a>
