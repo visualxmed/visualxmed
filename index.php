9f234007603234b9ebc4c5cebf51ec7607e7402a
aurinsomnia <auriskv@gmail.com>
2017-07-13 17:37:18 -0400

    "Removing btn-secondary characteristics"

--- index.php
+++ index.php
@@ -89,11 +89,11 @@
             </div>
 
             <div class="col col-md-9">
-                        <a href="index.php"><div class="btn active btn navbutton" role="button">Home</div></a>
-                        <a href="about.html"><div class="btn btn-secondary navbutton" role="button">About Us</div></a>
-                        <a href="mission.html"><div class="btn btn-secondary navbutton" role="button">Mission</div></a>
-                        <a href="#"><div class="btn btn-secondary navbutton" role="button">Opportunities</div></a>
-                        <a href="contact.html"><div class="btn btn-secondary navbutton" role="button">Contact Us</div></a>
+                        <a href="index.php"><div class="btn active navbutton" role="button">Home</div></a>
+                        <a href="about.html"><div class="btn navbutton" role="button">About Us</div></a>
+                        <a href="mission.html"><div class="btn navbutton" role="button">Mission</div></a>
+                        <a href="#"><div class="btn navbutton" role="button">Opportunities</div></a>
+                        <a href="contact.html"><div class="btn navbutton" role="button">Contact Us</div></a>
                 </div>
              </div> 
         </div>
